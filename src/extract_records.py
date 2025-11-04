b37ed2ff9a8ee8dc2d7aff594f9f2cb113b69276
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-11-04 08:59:00 +0000

    rename generate_tsv
    update doc statement

---
+++ src/extract_records.py
@@ -0,0 +1,94 @@
+"""
+Version 2.0 of TNA to EHRI ETL pipeline
+
+This will extract sets of individual records from Discovery and save them for later transformation into EAD XML to load into the EHRI Portal 
+
+"""
+import requests
+from pathlib import Path
+from time import sleep
+
+
+SERIES_PATH = Path(r"C:\Users\rbruno\OneDrive - The National Archives\Projects\EHRI\Data")
+SERIES_FILE = SERIES_PATH / "series.txt"
+
+DISCOVERY_API_URI = r"https://discovery.nationalarchives.gov.uk/API/"
+
+
+def get_records_from_api(series: str) -> list[dict]:
+    """
+    Queries the Discovery API for all the records in a given series, in order, 
+    uses nextBatchMark value to determine whether more records need to be retrieved. 
+
+    Args:
+        series (str): reference of the series e.g. "PREM 8"
+
+    Returns:
+        list[dict]: results in json
+    """    
+
+    batch_mark="*"
+    records = []
+    while batch_mark:
+        api_query = f"{DISCOVERY_API_URI}search/records?sps.recordSeries={series}&sps.searchQuery=*&sps.sortByOption=REFERENCE_ASCENDING&sps.resultsPageSize=1000&sps.batchStartMark={batch_mark}"   
+        result = requests.get(api_query)
+        if result.status_code != 200:
+            return
+
+        data = result.json()          
+        records.extend(data['records'])
+        batch_mark = data['nextBatchMark']
+        sleep(1)
+        
+    print(f"\tResult: {len(records)} records retrieved.")
+
+    return records
+       
+
+def create_series_links(series: str, records_from_api: list[dict]) -> list[str]:
+    """
+    Each record from the API is returned as a url along with the catalog id 
+    e.g., https://discovery.nationalarchives.gov.uk/API/records/v1/details/C9295	C9295
+    This is the format required for bulk import into EHRI portal
+
+    Args:
+        series (str): reference of the series e.g. "PREM 8" 
+        records_from_api (list[dict]): the json results from the api query
+
+    Returns:
+        list[str]: a list of urls with its record id
+    """    
+
+    return [
+        f"{DISCOVERY_API_URI}records/v1/details/{record['id']}\t{record['id']}"
+        for record in records_from_api
+    ]
+
+
+def write_tsv(series: str, series_links: list) -> None:
+    """
+    Write all the urls extractef from Discovery into a tsv-format file whgich will be used to bulk import into EHRI
+
+    Args:
+        series (str): reference of the series e.g. "PREM 8" 
+        series_links (list): a list of urls with its record id
+    """           
+    series_file_name = series.replace(" ", "_")
+
+    with open(SERIES_PATH / "output" / f"{series_file_name}.tsv", "w") as output:
+        output.write("\n".join(series_links))
+        print(f"\tData output to {series_file_name}.tsv\n")
+    
+
+if __name__ == "__main__":
+    if not Path(SERIES_PATH):
+        print("Invalid location for series.txt")
+        exit()
+
+    with open(SERIES_FILE, "r") as input:
+        for series in input.read().splitlines():
+            print(f"Series: {series}")
+            json_results = get_records_from_api(series)
+            links = create_series_links(series, json_results)
+            write_tsv(series, links)
+            
