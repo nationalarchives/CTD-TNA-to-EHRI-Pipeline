c45bd33dba82ca44d10ad9b6c1151971d15dfe60
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-11-11 17:27:49 +0000

    use file name as shelf key

--- tests/create_api_cache.py
+++ tests/create_api_cache.py
@@ -5,17 +5,18 @@ import shelve
 from extract_records import read_records_from_file, get_records_from_api, SERIES_PATH
 
 test_data_files = [
-    Path(SERIES_PATH / "TNA collections WO 311 German concentration camp staff_ready.xlsx"),
-    Path(SERIES_PATH / "TNA collections FO 950 Nazi persecution records_ready.xlsx)")
+    "TNA collections WO 311 German concentration camp staff_ready.xlsx",
+    "TNA collections FO 950 Nazi persecution records_ready.xlsx"
     ]
 
 with shelve.open("api_cache.db") as shelf:
     for tna_file in test_data_files:
-        tna_records: list[dict] = read_records_from_file(tna_file)
+        tna_records: list[dict] = read_records_from_file(Path(SERIES_PATH / tna_file))
         records_for_EHRI = get_records_from_api(tna_records)
 
         print(f"{len(records_for_EHRI)=}")
 
+        shelf[tna_file] = records_for_EHRI
     # shelf["last_updated"] = 1696846049.8469703
     # shelf["user_sessions"] = {
     #     "jdoe@domain.com": {
