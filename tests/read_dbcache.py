29fbc1915ad11bded3e8c8799b766c60491c286c
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-11-13 15:34:37 +0000

    new file to confirm cached records db

---
+++ tests/read_dbcache.py
@@ -0,0 +1,19 @@
+"""
+Script to read and summarize contents of the api_cache.db file
+created by create_api_cache.py
+Must be run in the same directory where api_cache.db is located
+"""
+import dbm
+import pickle
+
+
+with dbm.open("api_cache.db") as db:
+    print(db.keys())
+    for key, value in db.items():
+        records = pickle.loads(value)
+        total_records = 0
+        for group in records:
+            total_records += len(group)
+        print(f"{key.decode()=}: contains {total_records} records in {len(records)} group(s)")
+
+
