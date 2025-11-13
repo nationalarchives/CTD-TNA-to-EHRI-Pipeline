dc8be83300979986666bd6a2c14dd0d73e15796e
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-11-13 15:19:02 +0000

    add docstring

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,13 @@ import pickle
 
 @pytest.fixture()
 def cached_records():
+    """
+    uses data from api_cache.db to create fixture
+    api_cache.db is created by running create_api_cache.py
+
+    Returns:
+        [dict]: name of file containing ids of candidate records mapped to list of the actual records retrieved from Discovery API
+    """
     with dbm.open("api_cache.db") as db:
         print(db.keys())
         return {
