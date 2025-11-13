c945c47ffde8df1a2e4eb0ae2f183801696fff8d
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-11-13 15:14:59 +0000

    remove zombie code; add candidate file; add output message

--- tests/create_api_cache.py
+++ tests/create_api_cache.py
@@ -6,25 +6,15 @@ from extract_records import read_records_from_file, get_records_from_api, SERIES
 
 test_data_files = [
     "TNA collections WO 311 German concentration camp staff_ready.xlsx",
-    "TNA collections FO 950 Nazi persecution records_ready.xlsx"
+    "TNA collections FO 950 Nazi persecution records_ready 1-2111.xlsx",
+    "TNA collections rel to German War Criminals FO 371-104142-104156_ready.xlsx"
     ]
 
 with shelve.open("api_cache.db") as shelf:
     for tna_file in test_data_files:
+        print(f"Processing file: {tna_file}")
         tna_records: list[dict] = read_records_from_file(Path(SERIES_PATH / tna_file))
-        records_for_EHRI = get_records_from_api(tna_records)
+        records_for_cache = get_records_from_api(tna_records)
 
-        print(f"{len(records_for_EHRI)=}")
+        shelf[tna_file] = records_for_cache
 
-        shelf[tna_file] = records_for_EHRI
-    # shelf["last_updated"] = 1696846049.8469703
-    # shelf["user_sessions"] = {
-    #     "jdoe@domain.com": {
-    #         "user_id": 4185395169,
-    #         "roles": {"admin", "editor"},
-    #         "preferences": {
-    #             "language": "en_US",
-    #             "dark_theme": False
-    #         }
-    #     }
-    # }
