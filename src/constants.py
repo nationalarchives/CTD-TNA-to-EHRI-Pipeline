4ef9757bb40e43ab2e309a881e3a4c77a529daa9
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-11-14 11:02:13 +0000

    add module

---
+++ src/constants.py
@@ -0,0 +1,21 @@
+"""
+module for defining constants and constant namespaces.
+"""
+
+from pathlib import Path
+
+def DataFolders():
+    __slots__ = ()
+
+    _root = Path(r"C:\Users\rbruno\OneDrive - The National Archives\Projects\EHRI\Data")
+    INPUT = _root / "1-INPUT"
+    CACHE = _root / "2-CACHE/api_records.db"
+    TRANSFORM = _root / "3-TRANSFORM"
+    ARCHIVE = _root / "4-ARCHIVE"
+
+
+DATA = DataFolders()
+DISCOVERY_API_URI = r"https://discovery.nationalarchives.gov.uk/API"
+PAUSE_IN_SECONDS = 2
+PAGE_SIZE = 1000
+
