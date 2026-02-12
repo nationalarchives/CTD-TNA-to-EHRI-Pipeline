6233c6ab082883eabf251542746bcb61100146b8
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-02-12 11:11:49 +0000

    feat: change return to flat list instead of pages of records
    Pages only required when creating list of records to send to EHRI. For caching only flat list is needed - also easier to search

--- src/extract_records.py
+++ src/extract_records.py
@@ -142,39 +142,31 @@ def get_record_ids_from_api_with_catalogue_lineage(candidate_records: list[dict]
     return (records_by_lineage, local_cache)
 
 
-def get_records_from_api(candidate_records: list[dict]) -> list[list[dict]]: 
+def get_records_from_api(candidate_records: list[dict]) -> list[dict]: 
     """
     Uses list of records proposed for EHRI transfer and retrieves the full JSON records from Discovery.
-    The records are collated into smaller groups to ease EHRI import - each group will be transformed into one EAD XML
 
     Args:
         candidate_records (list[dict]): rows of data extracted from Excel
     Returns:
-        records_retrieved (list[list[dict]]): Discovery JSON records collated into smaller groups
+        records_retrieved (list[dict]): Discovery JSON records 
     """
     records_retrieved = []
-    page_of_records = []
     total_records_retrieved = 0
     for index, candidate in enumerate(candidate_records):
         record_id = candidate['id'] if 'id' in candidate else candidate['ID']
         
         if record := get_api_record(record_id):
             print(f"\tRetrieving record {index + 1} of {len(candidate_records)}: {record_id}")
-            page_of_records.append(record)
+            records_retrieved.append(record)
             total_records_retrieved += 1
         else:
             continue
 
         reached_end_of_page = ((index + 1) % PAGE_SIZE == 0)
-        reached_end_of_records = (index == len(candidate_records) - 1)
 
         if reached_end_of_page: 
-            records_retrieved.extend([page_of_records])
-            page_of_records = []
             sleep(PAUSE_IN_SECONDS)
-
-        elif reached_end_of_records:
-            records_retrieved.extend([page_of_records])
 
     print(f"-> Total records retrieved: {total_records_retrieved}\n")
         
