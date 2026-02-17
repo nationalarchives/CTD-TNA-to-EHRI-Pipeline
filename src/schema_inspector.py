538bb941238998aadd4a335f1ed0932054655d43
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-02-17 12:51:51 +0000

    feat: collect all occurences of schema in order to later calculate more detailed stats on used schemas

--- src/schema_inspector.py
+++ src/schema_inspector.py
@@ -11,11 +11,11 @@ pretty = pprint.PrettyPrinter(indent=4)
 regex = re.compile(r"""<colltype id="(?P<schema_name>.*?)">,<\/colltype>""")
 
 with shelve.open(DATA.CACHE, "r") as shelf:
-    schemas_found = set()
+    schemas_found = []
     for count, record in enumerate(shelf['records'].values(), start=1):
         if schema := record['scopeContent']['schema']:
             schema_name = regex.match(schema)['schema_name']
-            schemas_found.add(schema_name)
+            schemas_found.append(schema_name)
             print(f"{count=:<5}{' '*15}{record['id']=}\t{schema_name=}")
 
     pretty.pprint(schemas_found)
