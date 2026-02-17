a557c29a80eab4597ee625d0a9bdc85f6a9c9ec7
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-02-17 12:52:28 +0000

    feat: print statistics on discovered schemas

--- src/schema_inspector.py
+++ src/schema_inspector.py
@@ -1,6 +1,7 @@
 import shelve
 import pprint
 import re
+from collections import Counter
 
 from constants import DATA
 
@@ -18,5 +19,11 @@ with shelve.open(DATA.CACHE, "r") as shelf:
             schemas_found.append(schema_name)
             print(f"{count=:<5}{' '*15}{record['id']=}\t{schema_name=}")
 
-    pretty.pprint(schemas_found)
-    print(f"Total records in cache: {count=:<5}\nUnique schemas found: {len(schemas_found)}")
+    counter = Counter(schemas_found)
+    print(
+        f"Total records in cache: {count=:<5}\n"
+        f"Total schemas found: {len(counter.values())}\n"
+        f"Total records with schema: {counter.total()}\n"
+        f"Schemmas found", end=': \n'
+    )
+    pretty.pprint(dict(counter))
