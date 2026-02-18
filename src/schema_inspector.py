4923cd485ae8a946d5396231c0b2f9fa0eb1ce5c
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-02-18 09:30:23 +0000

    refactor: extract into method as a way of self-documenting and also saving for later use

--- src/schema_inspector.py
+++ src/schema_inspector.py
@@ -11,19 +11,23 @@ pretty = pprint.PrettyPrinter(indent=4)
 
 regex = re.compile(r"""<colltype id="(?P<schema_name>.*?)">,<\/colltype>""")
 
-with shelve.open(DATA.CACHE, "r") as shelf:
-    schemas_found = []
-    for count, record in enumerate(shelf['records'].values(), start=1):
-        if schema := record['scopeContent']['schema']:
-            schema_name = regex.match(schema)['schema_name']
-            schemas_found.append(schema_name)
-            print(f"{count=:<5}{' '*15}{record['id']=}\t{schema_name=}")
 
-    counter = Counter(schemas_found)
-    print(
-        f"Total records in cache: {count=:<5}\n"
-        f"Total schemas found: {len(counter.values())}\n"
-        f"Total records with schema: {counter.total()}\n"
-        f"Schemmas found", end=': \n'
-    )
-    pretty.pprint(dict(counter))
+def report_schema_statistics() -> None:
+    with shelve.open(DATA.CACHE, "r") as shelf:
+        schemas_found = []
+        for count, record in enumerate(shelf['records'].values(), start=1):
+            if schema := record['scopeContent']['schema']:
+                schema_name = regex.match(schema)['schema_name']
+                schemas_found.append(schema_name)
+                print(f"{count=:<5}{' '*15}{record['id']=}\t{schema_name=}")
+
+        counter = Counter(schemas_found)
+        print(
+            f"Total records in cache: {count=:<5}\n"
+            f"Total schemas found: {len(counter.values())}\n"
+            f"Total records with schema: {counter.total()}\n"
+            f"Schemmas found", end=': \n'
+        )
+        pretty.pprint(dict(counter))
+
+
